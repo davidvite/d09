917cebac260a34e229af4e85e3870aadaad895c4
David Vite Hernandez <dvite-he@e1z1r8p19.8.111.10.in-addr.arpa>
2016-07-22 16:56:50 -0700

    collatz 000001

---
+++ ex07/ft_collatz_conjecture.c
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_collatz_conjecture.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dvite-he <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/07/22 14:35:28 by dvite-he          #+#    #+#             */
+/*   Updated: 2016/07/22 14:35:32 by dvite-he         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+unsigned int	ft_collatz_conjecture(unsigned int base)
+{
+	if (base == 1)
+		return (0);
+	if (base % 2 == 0)
+		return (1 + ft_collatz_conjecture(base / 2));
+	return (1 + ft_collatz_conjecture((base * 3) + 1));
+}
